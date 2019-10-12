6e747af0fd5c08455b014e465db3077760b789c5
BlackPhrase <BlackPhrase@users.noreply.github.com>
2019-10-12 11:21:33 +0000

    The game module will be implemented in C#

---
+++ src/game/Game.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace RotaryEngine
+{
+	namespace Game
+	{
+		public class CGame : IGame
+		{
+			public CGame()
+			{
+			}
+
+			public bool Init()
+			{
+				return true;
+			}
+
+			public void Shutdown()
+			{
+			}
+
+			public void Update(float afTimeStep)
+			{
+			}
+		}
+	} // namespace Game
+} // namespace RotaryEngine
---
+++ src/game/IGame.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace RotaryEngine
+{
+	namespace Game
+	{
+		public interface IGame
+		{
+			bool Init();
+			void Shutdown();
+			void Update(float afTimeStep);
+		}
+	} // namespace Game
+} // namespace RotaryEngine
